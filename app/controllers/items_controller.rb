0a0a6349f55db1f30186f83c5055be9791bcee84
ieva jirgensone <ieva.ji.mail@gmail.com>
2022-09-27 10:16:18 +0200

    Refactored ItemsController and added math to items#create and #destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,43 +1,69 @@
 class ItemsController < ApplicationController
+  before_action :set_list, except: :destroy
+  before_action :set_window, only: :create
+  before_action :set_item, only: :destroy
+
   def new
-    @list = List.find(list_id)
     @item = current_user.items.build
     authorize @item
   end
 
   def create
-    @item ||= Item.new(name: item_name,
-                       value: value,
-                       list_id: list_id,
-                       user_id: current_user.id)
+    @item = current_user.items.build(item_params)
+    # @item.list_id = list_id <-- Alternative to custom hashing item_params
+    recalculate_value
     @item.save!
     authorize @item
     redirect_to list_path(list_id)
   end
 
   def destroy
-    @item = Item.find(item_id)
     authorize @item
-    list = @item.list.id
+    @list = current_user.lists.find(@item.list.id)
+    @window = @list.window
+    recalculate_value
     @item.destroy
-    redirect_to list_path(list)
+    redirect_to list_path(@list)
   end
 
   private
 
+  # CREATE
+  def recalculate_value
+    action = caller_locations.first.base_label
+    parents = [@list, @window]
+    parents.each do |parent|
+      action == "destroy" ? parent.current_value -= @item.value : parent.current_value += @item.value
+      parent.difference = parent.budget - parent.current_value
+      parent.difference.negative? ? parent.exceeded = true : parent.exceeded = false
+      parent.save!
+    end
+  end
+
+  # BEFORE_ACTION
+  def set_list
+    @list = current_user.lists.find(list_id)
+  end
+
+  def set_window
+    @window = @list.window
+  end
+
+  def set_item
+    @item = current_user.items.find(item_id)
+  end
+
+  # PARAMS
   def item_id
-    params[:id]
+    params.require(:id)
   end
 
   def list_id
-    params[:list_id]
+    params.require(:list_id)
   end
 
-  def item_name
-    params[:item][:name]
-  end
-
-  def value
-    params[:item][:value]
+  def item_params
+    data = params.require(:item).permit(:name, :value)
+    { name: data[:name], value: data[:value], list_id: list_id }
   end
 end
